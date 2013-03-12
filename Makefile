a00bb3c6d51138e9d93ae99c20d40843d6302c06
David Hovemeyer <david.hovemeyer@gmail.com>
2013-03-12 14:59:10 -0400

    implemented integer sum computation

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CFLAGS = -g -Wall -std=gnu99 -D_REENTRANT
 PROGS = hello
 
 hello : hello.o
-	$(CC) -o $@ hello.o
+	$(CC) -o $@ hello.o -lpthread
 
 clean :
 	rm -f *.o $(PROGS) *~ solution.zip submit.properties
--- hello.c
+++ hello.c
@@ -3,9 +3,69 @@
 #include <stdint.h>
 #include <pthread.h>
 
+#define NUMTHREADS  4
+
+// Work done by one thread
+struct Work {
+	int start; // first integer in range
+	int len;   // how many integers in range
+	int sum;   // result of thread's local computation
+};
+
+void *worker(void *arg)
+{
+	struct Work *work = arg;
+
+	// local computation
+	work->sum = 0;
+	for (int i = work->start; i < work->start + work->len; i++) {
+		work->sum += i;
+	}
+
+	return NULL;
+}
+
 int main(int argc, char **argv)
 {
-	// TODO
+	int max;
+
+	printf("Max int: " );
+	scanf("%i", &max);
+
+	// Divide up work
+	struct Work workList[NUMTHREADS];
+	pthread_t threadList[NUMTHREADS];
+
+	int chunkSize = max / NUMTHREADS;
+	for (int i = 0; i < NUMTHREADS; i++) {
+		workList[i].start = 1 + (i * chunkSize);
+		workList[i].len = chunkSize;
+		if (i == NUMTHREADS - 1) {
+			workList[i].len += (max % NUMTHREADS);
+		}
+	}
+
+	// Start threads
+	for (int i = 0; i < NUMTHREADS; i++) {
+		pthread_create(&threadList[i], NULL, &worker, &workList[i]);
+	}
+
+	printf("Threads started\n");
+
+	// Wait for threads to finish
+	for (int i = 0; i < NUMTHREADS; i++) {
+		pthread_join(threadList[i], NULL);
+	}
+
+	printf("Threads finished\n");
+
+	// Combine results
+	int global_sum = 0;
+	for (int i = 0; i < NUMTHREADS; i++) {
+		global_sum += workList[i].sum;
+	}
+
+	printf("Sum is %i\n", global_sum);
 
 	return 0;
 }
